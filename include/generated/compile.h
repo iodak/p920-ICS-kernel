2fb56994f3f9bd01764d708c5dbb775880a5b1be
iodak <o93.ivan@gmail.com>
2012-12-06 22:56:33 +0100

    OC RAM to 480

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Wed Dec 5 15:26:56 CET 2012"
+#define UTS_VERSION "#9 SMP PREEMPT Thu Dec 6 22:39:11 CET 2012"
 #define LINUX_COMPILE_BY "ivan"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
