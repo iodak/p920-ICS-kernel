4285630ddbf28d90eb89e17c6959ef3de8a67b2c
iodak <o93.ivan@gmail.com>
2012-12-27 18:25:54 +0100

    nothing

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 9 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP PREEMPT Thu Dec 6 22:39:11 CET 2012"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Dec 27 17:46:17 CET 2012"
 #define LINUX_COMPILE_BY "ivan"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
