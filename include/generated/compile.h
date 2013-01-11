00134c9efb49015e97f49628c345ec8295154bed
iodak <o93.ivan@gmail.com>
2013-01-11 23:14:13 +0100

    Added Fsync control by Ezekeel

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Fri Jan 11 21:54:03 CET 2013"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Jan 11 23:06:21 CET 2013"
 #define LINUX_COMPILE_BY "ivan"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.7.3 20121106 (prerelease) (crosstool-NG linaro-1.13.1-4.7-2012.11-20121123 - Linaro GCC 2012.11) "
